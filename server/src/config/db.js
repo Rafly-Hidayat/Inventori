8836f8f703066f70fd2cb991246a52d71a149186
rksrbbn <rakasantang22@gmail.com>
2021-04-23 11:39:08 +0700

    mengupdate koneksi

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,9 +2,13 @@
 const mysql = require('mysql')
 require('dotenv').config()
 
-module.exports = mysql.createConnection({
+const con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
+})
+
+con.connect((err) => {
+ err ? console.log("database not connected.") : console.log('connected to database')
 })
