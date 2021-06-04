754177eed5c0ef8e1fd4e444cacd442c98eca29e
Rafly-Hidayat <rafly.hidayat2710@gmail.com>
2021-06-04 14:58:57 +0700

    mengubah pagination data admin, mengubah get tampilan data pembelian, dan memperbaiki data transaction pada tabel pembelian

--- server/src/controllers/pembelianController.js
+++ server/src/controllers/pembelianController.js
@@ -1,15 +1,74 @@
 const Pembelian = require('../models/Pembelian')
 
 module.exports = {
-    getAll: (req, res) => {
-		Pembelian.getAll(req.con, req.query, res)
-	},
+    getAll : (req, res) => {
+        let page = req.query.page || 1
+        let limit = req.query.limit || 3
+        let offset = ( page - 1 ) * limit
+        
+        Pembelian.getAll(req.con, req.query, limit, offset, (err, rows) => {
+            if(err) throw (err)
+            Pembelian.get(req.con, (err, results) => {
+                if(err) throw (err)
+                const pageLimit = Math.ceil(results.length/parseInt(limit))
+                res.json({page: `${page} of ${pageLimit}`, result:rows.length, data: rows})
+            })
+        })
+    },
 
 	getById: (req, res) => {
 		Pembelian.getById(req.con, req.params.kd_pembelian, (err, rows) => {
 			if(err) throw err
 			res.json(rows)
 		})
+	},
+
+    getAllDetail : (req, res) => {
+        let page = req.query.page || 1
+        let limit = req.query.limit || 3
+        let offset = ( page - 1 ) * limit
+        
+        Pembelian.getAllDetail(req.con, req.query, limit, offset, (err, rows) => {
+            if(err) throw (err)
+            Pembelian.getDetail(req.con, (err, results) => {
+                if(err) throw (err)
+                const pageLimit = Math.ceil(results.length/parseInt(limit))
+                res.json({page: `${page} of ${pageLimit}`, result:rows.length, data: rows})
+            })
+        })
+    },
+
+	getDetailById: (req, res) => {
+		Pembelian.getDetailById(req.con, req.params.kd_pembelian, (err, rows) => {
+			if(err) throw err
+			res.json(rows)
+		})
+	},
+
+    getAllBarang : (req, res) => {
+        let page = req.query.page || 1
+        let limit = req.query.limit || 3
+        let offset = ( page - 1 ) * limit
+        
+        Pembelian.getAllBarang(req.con, req.query, limit, offset, (err, rows) => {
+            if(err) throw (err)
+            Pembelian.getBarang(req.con, (err, results) => {
+                if(err) throw (err)
+                const pageLimit = Math.ceil(results.length/parseInt(limit))
+                res.json({page: `${page} of ${pageLimit}`, result:rows.length, data: rows})
+            })
+        })
+    },
+
+	getBarangById: (req, res) => {
+		Pembelian.getBarangById(req.con, req.params.kd_pembelian, (err, rows) => {
+			if(err) throw err
+			res.json(rows)
+		})
+	},
+
+	transaction: (req, res) => {
+		Pembelian.transaction(req.con, req.body, res)
 	}
 
 }
--- server/src/routes/pembelianRouter.js
+++ server/src/routes/pembelianRouter.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const pembelianController = require('../controllers/pembelianController')
-const pembelian = require('../middleware/pembelian')
 
     router.get('/pembelian', pembelianController.getAll)
 
     router.get('/pembelian/:kd_pembelian', pembelianController.getById)
 
-    // router.post('/pembelian', pembelianController.transaction)
+    router.get('/detail', pembelianController.getAllDetail)
 
-    router.post('/pembelian', pembelian.transaction)
+    router.get('/detail/:id_pembelian', pembelianController.getDetailById)
+
+    router.get('/barang_pembelian', pembelianController.getAllBarang)
+
+    router.get('/barang_pembelian/:kd_barang_beli', pembelianController.getBarangById)
+
+    router.post('/pembelian', pembelianController.transaction)
 
 module.exports = router
